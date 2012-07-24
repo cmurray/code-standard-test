f4764adb0f96c0ea00f865e72664328d83883f5b
Chris M <cmurray@creatuity.com>
2012-07-24 13:23:04 -0500

    Took out non phpmd and phpcs non -ci

--- coding-standard-test.php
+++ coding-standard-test.php
@@ -5,18 +5,4 @@
 //Another comment
 //A normal comment
 
-
-class someClass {
-
-    private function classFunction() {}
-    private function bar($howdy) {}
-}
-
-class someOtherClass {
-
-    private function classFunction() {}
-    private function bar($howdy) {}
-}
-
-
 ?>
