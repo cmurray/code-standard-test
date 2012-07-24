2ae321464554a2c4d5d386c8f8fde5f2213caabf
Chris M <cmurray@creatuity.com>
2012-07-24 16:47:10 -0500

    ejfhakjrgesr

--- coding-standard-test.php
+++ coding-standard-test.php
@@ -2,8 +2,13 @@
 //Hur be a comment
 //Another comment
 //A normal comment
-#A normal comment
+//A normal comment
 //A normal comment
 
+class someClass {
+
+    protected $thisIsAReallyLongNameForAVariable = 5;
+    public function a($index) {}
+}
 
 ?>
