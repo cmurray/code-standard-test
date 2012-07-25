7bdfd9eddea07f640437d8e75ff992d128381cbe
Chris M <cmurray@creatuity.com>
2012-07-25 10:57:03 -0500

    Trying to make phpdox be all fancy

--- coding-standard-test.php
+++ coding-standard-test.php
@@ -5,6 +5,16 @@
 //A normal comment
 //A normal comment
 
+
+/**
+ *This is the first line
+ *
+ *This is the second paragraph that
+ *is kind of long, but it is supposed to
+ *be this long I think.
+ *
+ */
+
 class someClass {
 
     protected $thisIsAReallyLongNameForAVariable = 5;
