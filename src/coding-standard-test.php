60ddb16497761386dcdcfd7676f18c47385f9512
Chris M <cmurray@creatuity.com>
2012-07-26 13:04:17 -0500

    Testing docblock

--- src/coding-standard-test.php
+++ src/coding-standard-test.php
@@ -5,6 +5,16 @@
 //A normal comment
 //A normal comment
 
+/**
+ * Short desc
+ *
+ * Long description first sentence starts here
+ * and continues on this line for a while
+ * finally concluding here at the end of
+ * this paragraph
+ *
+ * The blank line above denotes a paragraph break
+ */
 class someClass {
 
     protected $thisIsAReallyLongNameForAVariable = 5;
