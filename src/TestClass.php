f57e0440d797c78c058283b04eba0a2e9cdc14ac
Chris M <cmurray@creatuity.com>
2012-08-08 18:02:43 -0500

    fixing tabs

--- src/TestClass.php
+++ src/TestClass.php
@@ -6,17 +6,17 @@ class TestClass {
     public $PublicCamelFail;
     public $_publicUnderscoreFail;
     public $_PublicUnderscoreCamelFail;
-	
+
     private $privateUnderscoreFail;
     private $PrivateUnderscoreCamelFail;
     private $_privatePass;
     private $_PrivateCamelFail;
-	
+
     protected $protectedUnderscoreFail;
     protected $ProtectedUnderscoreCamelFail;
     protected $_protectedPass;
     protected $_ProtectedCamelFail;
-	
+
 
     public function publicPass() {}
     
