48fe859e2bd0d1b41940522eab2564be398e27de
Chris M <cmurray@creatuity.com>
2012-08-08 17:59:07 -0500

    gg

--- src/TestClass.php
+++ src/TestClass.php
@@ -2,6 +2,22 @@
 
 class TestClass {
 
-    public function publicPass() {}                       //Passes correctly
+	public $publicPass;
+	public $PublicCamelFail;
+	public $_publicUnderscoreFail;
+	public $_PublicUnderscoreCamelFail;
+	
+	private $privateUnderscoreFail;
+	private $PrivateUnderscoreCamelFail;
+	private $_privatePass;
+	private $_PrivateCamelFail;
+	
+	protected $protectedUnderscoreFail;
+	protected $ProtectedUnderscoreCamelFail;
+	protected $_protectedPass;
+	protected $_ProtectedCamelFail;
+	
+
+    public function publicPass() {}
     
 }
