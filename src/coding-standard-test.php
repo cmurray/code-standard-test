6c2bed00653294c0491443480d86f73379ae0617
Chris M <cmurray@creatuity.com>
2012-08-08 17:06:49 -0500

    fdsf

--- src/coding-standard-test.php
+++ src/coding-standard-test.php
@@ -9,14 +9,15 @@ class TestClass {
     public function PublicCamelFail() {}    
     private function PrivateUnderscoreCamelFail() {}
     protected function ProtectedUnderscoreCamelFail() {}
-/*
+
 
     public function _publicUnderscoreFail() {}    
     private function _privatePass() {}
     protected function _protectedPass() {}
     
+    public function _PublicUnderscoreCamelFail() {}
     private function _PrivateCamelFail() {}
     protected function _ProtectedCamelFail() {}
 
-*/    
+    
 }
