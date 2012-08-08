c9b1da9f93004780e25814dd7faa435f6c5ffe95
Chris M <cmurray@creatuity.com>
2012-08-08 17:41:33 -0500

    hsf

--- src/coding-standard-test.php
+++ src/coding-standard-test.php
@@ -1,23 +1,7 @@
 <?php
 
-class TestClass {
+class testClass {
 
-    public function publicPass() {}    
-    private function privateUnderscoreFail() {}
-    protected function protectedUnderscoreFail() {}
-
-    public function PublicCamelFail() {}    
-    private function PrivateUnderscoreCamelFail() {}
-    protected function ProtectedUnderscoreCamelFail() {}
-
-
-    public function _publicUnderscoreFail() {}    
-    private function _privatePass() {}
-    protected function _protectedPass() {}
-    
-    public function _PublicUnderscoreCamelFail() {}
-    private function _PrivateCamelFail() {}
-    protected function _ProtectedCamelFail() {}
-
+    public function publicPass() {}                       //Passes correctly
     
 }
