819b6520e986acdac9ec8b51c2bfdf8d4ce1c9e3
Chris M <cmurray@creatuity.com>
2012-08-09 10:46:48 -0500

    I suck at php

--- src/TestClass.php
+++ src/TestClass.php
@@ -2,10 +2,10 @@
 
 class TestClass {
 
-	const $PASS = 5;
-	const $FAIL = 4;
-	const $UNDERSCORE_PASS = 6;
-	const $UNDERSCORE_fail = 3;
+	const PASS = 5;
+	const FAIL = 4;
+	const UNDERSCORE_PASS = 6;
+	const UNDERSCORE_fail = 3;
 
 
     public function publicPass() {}
