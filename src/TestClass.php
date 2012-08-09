61749a50b1fa9b713103bb223948e0e015f911cf
Chris M <cmurray@creatuity.com>
2012-08-09 10:43:33 -0500

    defining constante fail

--- src/TestClass.php
+++ src/TestClass.php
@@ -2,10 +2,10 @@
 
 class TestClass {
 
-	constant $PASS = 5;
-	constant $FAIL = 4;
-	constant $UNDERSCORE_PASS = 6;
-	constant $UNDERSCORE_fail = 3;
+	define("PASS",1);
+	define("FAIl",2);
+	define("UNDERSCORE_PASS",3);
+	define("UNDERSCORE_fail",4);
 
 
     public function publicPass() {}
