3051ac3dec14c956d5457fc4bd9bbf43bb6abb15
Chris M <cmurray@creatuity.com>
2012-08-09 11:08:49 -0500

    gjh

--- src/AbstractClass.php
+++ src/AbstractClass.php
@@ -1,7 +1,6 @@
 <?php
 
-abstract class AbstractClass
-{
+abstract class AbstractClass {
     // Force Extending class to define this method
     abstract protected function getValue();
     abstract protected function prefixValue($prefix);
