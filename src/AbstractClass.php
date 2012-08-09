4711d34c76715d8075272f976e9e291663139a65
Chris M <cmurray@creatuity.com>
2012-08-09 11:19:22 -0500

    hh

--- src/AbstractClass.php
+++ src/AbstractClass.php
@@ -3,7 +3,7 @@
 abstract class AbstractClass {
     // Force Extending class to define this method
     abstract protected function _getValue();
-    abstract protected function _prefixValue($prefix);
+    abstract private function _prefixValue($prefix);
 
     // Common method
     public function printOut() {
