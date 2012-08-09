117cb755883bcad0315a558d78dd2ad4600bae60
Chris M <cmurray@creatuity.com>
2012-08-09 11:06:41 -0500

    dsfsd

---
+++ src/TestClass.php
@@ -0,0 +1,12 @@
+<?php
+
+abstract class AbstractClass {
+    // Force Extending class to define this method
+    abstract protected function getValue();
+    abstract protected function prefixValue($prefix);
+
+    // Common method
+    public function printOut() {
+        print $this->getValue() . "\n";
+    }
+}
